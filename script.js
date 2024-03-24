d70ff891b15ecbe56a4f67acf5a6376cb27bfafc
shoaib inamdar <shoaibinamdar14@gmail.com>
2024-03-24 11:17:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true,
+//     // lerp:.02
+// });
+
+var tl=gsap.timeline()
+tl.to(".yellow",{
+    top:"-100%",
+    duration:.5,
+    delay:.4,
+    ease:"expo.out"
+})
+tl.to(".loader video",{
+    top:"-100%",
+    duration:.5,
+    delay:1.5,
+    ease:"expo.out"
+})
+tl.to(".loader",{
+    top:"-100%",
+    duration:.5,
+    // delay:.5,
+    ease:"expo.out"
+})
+tl.to(".loader",{
+    display:'none',
+    opacity:0
+})
+
+var elems=document.querySelectorAll(".elem")
+var page2=document.querySelector(".page2")
+elems.forEach(function(ele){
+    ele.addEventListener("mouseenter",()=>{
+        var bgimg=ele.getAttribute("data-img")
+        page2.style.backgroundImage=`url(${bgimg})`
+    })
+})
